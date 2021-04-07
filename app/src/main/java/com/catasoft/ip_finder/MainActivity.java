f29991902fa59dbdb348f2aedc302161a60848b2
Marius-RO <blw_matt@rocketmail.com>
2021-04-07 14:24:27 +0300

    Add view models

--- app/src/main/java/com/catasoft/ip_finder/MainActivity.java
+++ app/src/main/java/com/catasoft/ip_finder/MainActivity.java
@@ -1,38 +1,69 @@
 package com.catasoft.ip_finder;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 import androidx.navigation.ui.NavigationUI.*;
 
+import com.catasoft.ip_finder.data.entities.SearchInfo;
 import com.catasoft.ip_finder.databinding.ActivityMainBinding;
+import com.catasoft.ip_finder.ui.history.HistoryViewModel;
+import com.catasoft.ip_finder.ui.search.SearchViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private HistoryViewModel historyViewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setLifecycleOwner(this);
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        binding.setLifecycleOwner(this);
+//
+//        NavHostFragment navHostFragment =
+//                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+//        if (navHostFragment != null) {
+//            NavController navController = navHostFragment.getNavController();
+//
+//            NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+//
+////            NavigationUI.setupActionBarWithNavController(this, navController);
+//        }
 
-        NavHostFragment navHostFragment =
-                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        if (navHostFragment != null) {
-            NavController navController = navHostFragment.getNavController();
+        setViewModel();
 
-            NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
 
-//            NavigationUI.setupActionBarWithNavController(this, navController);
-        }
+    private void setViewModel(){
+        // set ViewModel
+        historyViewModel = new ViewModelProvider(this).get(HistoryViewModel.class);
 
+        // set observers
+        historyViewModel.getLiveToastMessage().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        historyViewModel.getAllLiveSearches().observe(this, new Observer<List<SearchInfo>>() {
+            @Override
+            public void onChanged(List<SearchInfo> searches) {
+                historyViewModel.checkEmptyMode(searches);
+            }
+        });
     }
 }
--- app/src/main/java/com/catasoft/ip_finder/ui/history/HistoryViewModel.java
+++ app/src/main/java/com/catasoft/ip_finder/ui/history/HistoryViewModel.java
@@ -1,7 +1,61 @@
 package com.catasoft.ip_finder.ui.history;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class HistoryViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.catasoft.ip_finder.data.entities.SearchInfo;
+import com.catasoft.ip_finder.data.repository.SearchInfoRepository;
+
+import java.util.List;
+
+public class HistoryViewModel extends AndroidViewModel {
+
+    protected final SearchInfoRepository searchInfoRepository;
+
+    // By default, the empty mode will be set to false in order to avoid showing him before
+    // the first loading of the adapter list.
+    private final MutableLiveData<Boolean> liveEmptyMode = new MutableLiveData<>(false);
+
+    // Leave this with no initial value. If you put an initial value it will trigger setValue().
+    private final MutableLiveData<String> liveToastMessage = new MutableLiveData<>();
+
+    public HistoryViewModel(@NonNull Application application) {
+        super(application);
+        searchInfoRepository = new SearchInfoRepository(application);
+    }
+
+    public LiveData<String> getLiveToastMessage() { return liveToastMessage; }
+    public LiveData<Boolean> getLiveEmptyMode() { return liveEmptyMode; }
+
+    public void insert(SearchInfo value) {
+        searchInfoRepository.insert(value);
+    }
+
+    public void update(SearchInfo value) {
+        searchInfoRepository.update(value);
+    }
+
+    public void delete(SearchInfo value) {
+        searchInfoRepository.delete(value);
+    }
+
+    public void deleteAll(List<SearchInfo> items){
+        searchInfoRepository.deleteAll(items);
+    }
+
+    public LiveData<SearchInfo> getLiveSearchInfo(long searchId){ return searchInfoRepository.getLiveSearchInfo(searchId); }
+
+    public LiveData<List<SearchInfo>> getAllLiveSearches(){ return searchInfoRepository.getAllLiveSearches(); }
+
+    public void checkEmptyMode(@NonNull List<SearchInfo> currentList) {
+        if(currentList.isEmpty()){
+            liveEmptyMode.setValue(true);
+            return;
+        }
+        liveEmptyMode.setValue(false);
+    }
 }
